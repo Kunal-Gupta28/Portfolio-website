adad7fd970ac2c8729c4d5d957daf031252939e7
Kunal-Gupta28 <kunal.gmail.91165@gmail.com>
2026-03-29 17:00:59 +0530

    change cors

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,33 @@ const contactRoute = require("./routes/contact.route.js");
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://portfolio-website-chi-gilt.vercel.app",
+  "http://localhost:5173"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.use("/api", contactRoute);
 
-// 404 handler – must be AFTER all routes
+// 404 handler
 app.use((req, res) => {
-
   res.status(404).json({
     success: false,
     message: `${req.path} not found`,
